1186bcddad4b3f426f48ed12891687331c497572
Kevin Pan <kpan5121@gmail.com>
2025-06-25 21:13:34 +1000

    Client.py changes: Function changes: - start_connections:     Function is to be removed in the future.     Served as base for sending messages and testing socket networks - create_connection:     New function which is to be expanded and to serve as basis for sending user messages     Currently supporting infinite user input to send to server.
    Next steps:
        Add wrapper for commandline arguments, indicating to user on correct usage of the client
        Add error checking when attempting to connect to a port. Exit gracefully and display relevant error messages to user

--- Client/client.py
+++ Client/client.py
@@ -4,7 +4,7 @@ import selectors
 import types
 
 sel = selectors.DefaultSelector()
-messages = [b"Message 1 from client.", b"Message 2 from client."]
+messages = [b"Hi this is the client", b"Hi, another message from client!"]
 
 def start_connections(host, port, num_conns):
     server_addr = (host, port)
@@ -23,3 +23,26 @@ def start_connections(host, port, num_conns):
             outb=b"",
         )
         sel.register(sock, events, data=data)
+
+def create_connection(host, port):
+    server_addr = (host, port)
+
+    try:
+        print(f"Starting connection 1 to {server_addr}")
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect(server_addr) # Connect to specified host and port
+                                  # Currently testing on localhost only
+        while True:
+            a = input("Type a message to send: ")
+            sock.send(a.encode())
+            print(sock.recv(1024).decode())
+        # sock.send("Hello from client".encode())
+
+    except KeyboardInterrupt:
+        print("Interrupted, aborting...")
+
+# Soon, wrap this around with error checking and print error messages
+# host = sys.argv[1] 
+port = int(sys.argv[1]) # Currently testing on localhost only
+# num_conns = int(sys.argv[3])
+create_connection("127.0.0.1", port)
