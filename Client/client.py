0a6e0f5435feefd8114252c6e5cfb04a2317609d
Kevin Pan <kpan5121@gmail.com>
2025-06-26 19:50:39 +1000

    client.py changes: New class TestCases:     - Moved function start_connections to this class. Renamed to test_multiple_connections     - Class is to contain only functions used for bug and functionality testing
    Function changes:
        create_connections:
        - Added error checking for creating sockets
        - Error checking of connections will exit with appropriate error code
    
    Bug fixes:
    Serverside bug is confirmed cause by client
        - Whenever client attempts to send an empty message to server, the client hangs indefinietly
        - Fixed by checking for empty input strings, and notifying user that empty messages are not allowed
    
    Jobs completed:
        - Error checking of port number/availability when attempting to connect
    
    Jobs underway:
        - Limit accepted port range of client

--- Client/client.py
+++ Client/client.py
@@ -3,46 +3,81 @@ import socket
 import selectors
 import types
 
-sel = selectors.DefaultSelector()
-messages = [b"Hi this is the client", b"Hi, another message from client!"]
+from clientSupport import *
 
-def start_connections(host, port, num_conns):
-    server_addr = (host, port)
-    for i in range(0, num_conns):
-        connid = i + 1
-        print(f"Starting connection {connid} to {server_addr}")
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.setblocking(False)
-        sock.connect_ex(server_addr)
-        events = selectors.EVENT_READ | selectors.EVENT_WRITE
-        data = types.SimpleNamespace(
-            connid=connid,
-            msg_total=sum(len(m) for m in messages),
-            recv_total=0,
-            messages=messages.copy(),
-            outb=b"",
-        )
-        sel.register(sock, events, data=data)
+class TestCases:
+    sel = selectors.DefaultSelector()
+
+    def __init__(self, host, port, num_conns) -> types.NoneType:
+        self._host = host
+        self._port = port
+        self._num_conns = num_conns
+
+    def test_multiple_connections(host, port, num_conns) -> None:
+        messages = [b"Hi this is the client", b"Hi, another message from client!"]
+        server_addr = (host, port)
+        for i in range(0, num_conns):
+            connid = i + 1
+            print(f"Starting connection {connid} to {server_addr}")
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.setblocking(False)
+            sock.connect_ex(server_addr)
+            events = selectors.EVENT_READ | selectors.EVENT_WRITE
+            data = types.SimpleNamespace(
+                connid=connid,
+                msg_total=sum(len(m) for m in messages),
+                recv_total=0,
+                messages=messages.copy(),
+                outb=b"",
+            )
+            sel.register(sock, events, data=data)
 
 def create_connection(host, port):
     server_addr = (host, port)
-
     try:
-        print(f"Starting connection 1 to {server_addr}")
+        print(f"Initialising connection to {server_addr}")
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect(server_addr) # Connect to specified host and port
+        if sock == -1:
+            print("Could not establish socket") # Should rarely/never happen
+            return
+        
+        # Attempt to connect to server
+        try:
+            sock.connect(server_addr) # Connect to specified host and port
                                   # Currently testing on localhost only
+        except ConnectionRefusedError:
+            print(PORT_ERROR)
+            exit(PORT_EXIT)
+
+        
         while True:
             a = input("Type a message to send: ")
-            sock.send(a.encode())
-            print(sock.recv(1024).decode())
-        # sock.send("Hello from client".encode())
+            if len(a) == 0: # Filter out empty messages which hang the client
+                print("Cannot send empty messages!")
+            else:
+                sock.send(a.encode())
+                receive = sock.recv(1024).decode() # Currently only support receiving 1024 bytes
+                                               # Future addition: allow larger transmission of reasonable size
+                print(f"Server sent: {receive}")
+            
+
 
     except KeyboardInterrupt:
         print("Interrupted, aborting...")
+        sys.exit(1) # Exit client
 
-# Soon, wrap this around with error checking and print error messages
-# host = sys.argv[1] 
-port = int(sys.argv[1]) # Currently testing on localhost only
-# num_conns = int(sys.argv[3])
-create_connection("127.0.0.1", port)
+def main():
+    # host = sys.argv[1] # Only using localhost for current version
+
+    if len(sys.argv) < 2:
+        print(USAGE_ERROR)
+        exit(USAGE_EXIT) # Exit code 8
+
+    port = int(sys.argv[1]) # Get port specified by user
+    
+    create_connection("127.0.0.1", port)    
+    
+    return 0
+
+if __name__ == "__main__":
+    main()
